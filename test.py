454e9ebba21087100c322eecf049177eaddaea26
Prophet Xu <slowfishcolor@gmail.com>
2018-01-10 14:22:29 +0800

    add performance test

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ def serialize_test(rounds, payload):
         util.serialize_payload(payload)
     end = util.current_timestamp()
     print("json serialize time: %d" % (end - start))
+    return end - start
 
 
 def deserialize_test(rounds, json_str):
@@ -15,6 +16,7 @@ def deserialize_test(rounds, json_str):
         util.deserialize_payload(json_str)
     end = util.current_timestamp()
     print("json deserialize time: %d" % (end - start))
+    return end - start
 
 option = model.Option()
 option.password = "password"
@@ -41,7 +43,7 @@ analogJsonStr = util.serialize_payload(analogPayload)
 controlJsonStr = util.serialize_payload(controlPayload)
 instructionJsonStr = util.serialize_payload(instructionPayload)
 
-rounds = 10000
+rounds = 100000
 
 
 print(analogJsonStr)
@@ -50,25 +52,59 @@ controlobj = util.deserialize_payload(controlJsonStr)
 instructionobj = util.deserialize_payload(instructionJsonStr)
 
 
+seControlAvg = 0
+seAnalogAvg = 0
+seInsAvg = 0
+
+deControlAvg = 0
+deAnalogAvg = 0
+deInsAvg = 0
+
+serializeAvg = 0
+deserializeAvg = 0
+
 print("start serialize test")
 for i in range (1, 10):
     print("analogPayload")
-    serialize_test(rounds, analogPayload)
+    seAnalogAvg += serialize_test(rounds, analogPayload)
     print("controlPayload")
-    serialize_test(rounds, controlPayload)
+    seControlAvg += serialize_test(rounds, controlPayload)
     print("instructionPayload")
-    serialize_test(rounds, instructionPayload)
+    seInsAvg += serialize_test(rounds, instructionPayload)
 print("end serialize test")
 
 
 print("start deserialize test")
 for i in range(1, 10):
     print("analogPayload")
-    deserialize_test(rounds, analogJsonStr)
+    deAnalogAvg += deserialize_test(rounds, analogJsonStr)
     print("controlPayload")
-    deserialize_test(rounds, controlJsonStr)
+    deControlAvg += deserialize_test(rounds, controlJsonStr)
     print("instructionPayload")
-    deserialize_test(rounds, instructionJsonStr)
+    deInsAvg += deserialize_test(rounds, instructionJsonStr)
 print("end deserialize test")
 
 
+seAnalogAvg /= 10
+seControlAvg /= 10
+seInsAvg /= 10
+
+deAnalogAvg /= 10
+deControlAvg /= 10
+deInsAvg /= 10
+
+serializeAvg = (seAnalogAvg + seControlAvg + seInsAvg) / 3
+deserializeAvg = (deAnalogAvg + deControlAvg + deInsAvg) / 3
+
+print("")
+print("seAnalogAvg: ", seAnalogAvg)
+print("seControlAvg: ", seControlAvg)
+print("seInsAvg: ", seInsAvg)
+print("")
+print("deAnalogAvg: ", deAnalogAvg)
+print("deControlAvg: ", deControlAvg)
+print("deInsAvg: ", deInsAvg)
+print("")
+print("serializeAvg: ", serializeAvg)
+print("deserializeAvg: ", deserializeAvg)
+print("")
